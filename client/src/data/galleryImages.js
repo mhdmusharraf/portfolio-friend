5d398e67647ed20661c9e4565342850f07b35dfa
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2025-04-07 04:12:27 +0530

    testing

---
+++ client/src/data/galleryImages.js
@@ -0,0 +1,13 @@
+export const galleryImages = {
+    "wedding-engagement": [
+      { type: "image", src: "/images/wedding1.jpg" },
+      { type: "image", src: "/images/wedding2.jpg" },
+    ],
+    "video-edits": [
+      { type: "video", src: "/videos/edit1.mp4" },
+      { type: "video", src: "/videos/edit2.mp4" },
+      { type: "video", src: "/videos/edit3.mp4" },
+    ],
+    // other categories...
+  };
+  
