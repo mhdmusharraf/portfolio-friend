47f54b25dd6de81e8f50149d5b1494913dd5f992
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2025-04-06 15:29:11 +0530

    Maternity photos added

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -86,8 +86,34 @@ import puberty43 from '../assets/Puberty/pu43.jpg'
 
 
 
-// import maternity1 from '../assets/Maternity1.jpg'
-// import maternity2 from '../assets/Maternity2.jpg'
+ import maternity1 from '../assets/Maternity/m1.jpg'
+ import maternity2 from '../assets/Maternity/m2.jpg'
+ import maternity3 from '../assets/Maternity/m3.jpg'
+ import maternity4 from '../assets/Maternity/m4.jpg'
+ import maternity5 from '../assets/Maternity/m5.jpg'
+ import maternity6 from '../assets/Maternity/m6.jpg'
+ import maternity7 from '../assets/Maternity/m7.jpg'
+ import maternity8 from '../assets/Maternity/m8.jpg'
+ import maternity9 from '../assets/Maternity/m9.jpg'
+ import maternity10 from '../assets/Maternity/m10.jpg'
+ import maternity11 from '../assets/Maternity/m11.jpg'
+ import maternity12 from '../assets/Maternity/m12.jpg'
+ import maternity13 from '../assets/Maternity/m13.jpg'
+ import maternity14 from '../assets/Maternity/m14.jpg'
+ import maternity15 from '../assets/Maternity/m15.jpg'
+ import maternity16 from '../assets/Maternity/m16.jpg'
+ import maternity17 from '../assets/Maternity/m17.jpg'
+ import maternity18 from '../assets/Maternity/m18.jpg'
+ import maternity19 from '../assets/Maternity/m19.jpg'
+ import maternity20 from '../assets/Maternity/m20.jpg'
+ import maternity21 from '../assets/Maternity/m21.jpg'
+ import maternity22 from '../assets/Maternity/m22.jpg'
+ import maternity23 from '../assets/Maternity/m23.jpg'
+ import maternity24 from '../assets/Maternity/m24.jpg'
+
+
+
+
 // import birthday1 from '../assets/Birthday1.jpg'
 // import birthday2 from '../assets/Birthday2.jpg'
 
@@ -123,7 +149,7 @@ import puberty43 from '../assets/Puberty/pu43.jpg'
 const galleryImages = {
   'wedding-engagement': [wedding1, wedding2,wedding3,wedding4,wedding5,wedding6,wedding7,wedding8,wedding9,wedding10,wedding11,wedding12,wedding13,wedding14,wedding15,wedding16,wedding17,wedding18,wedding19,wedding20,wedding21,wedding22,wedding23,wedding24,wedding25,wedding26,wedding27,wedding28,wedding29,wedding30,wedding31,wedding32,wedding33,wedding34,wedding35 ],
    'puberty-ceremony': [puberty1, puberty2,puberty3,puberty4,puberty5,puberty6,puberty7,puberty8,puberty9,puberty10,puberty11,puberty12,puberty13,puberty14,puberty15,puberty16,puberty17,puberty18,puberty19,puberty20,puberty21,puberty22,puberty23,puberty24,puberty25,puberty26,puberty27,puberty28,puberty29,puberty30,puberty31,puberty32,puberty33,puberty34,puberty35,puberty36,puberty37,puberty38,puberty39,puberty40,puberty41,puberty42,puberty43],
-//   'maternity-shoot': [maternity1, maternity2],
+   'maternity-shoot': [maternity1, maternity2,maternity3,maternity4,maternity5,maternity6,maternity7,maternity8,maternity9,maternity10,maternity11,maternity12,maternity13,maternity14,maternity15,maternity16,maternity17,maternity18,maternity19,maternity20,maternity21,maternity22,maternity23,maternity24],
 //   'birthday-shoot': [birthday1, birthday2],
   'bridal-photography': [bridal1, bridal2,bridal3,bridal4,bridal5,bridal6,bridal7,bridal8,bridal9,bridal10,bridal11,bridal12,bridal13,bridal14,bridal15,bridal16,bridal17,bridal18],
 //   'product-photography': [product1, product2],
