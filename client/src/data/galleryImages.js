eb8e77dcc601c65ed9d4f7928e79eb99c3251386
mhdmusharraf <mhdmusharraf.edu@gmail.com>
2025-04-07 06:01:13 +0530

    galleryImages.js updated

--- client/src/data/galleryImages.js
+++ client/src/data/galleryImages.js
@@ -1,13 +1,423 @@
+import wedding1 from "../assets/Wedding/01.jpg";
+import wedding2 from "../assets/Wedding/02.jpg";
+import wedding3 from "../assets/Wedding/03.jpg";
+import wedding4 from "../assets/Wedding/04.jpg";
+import wedding5 from "../assets/Wedding/05.jpg";
+import wedding6 from "../assets/Wedding/06.jpg";
+import wedding7 from "../assets/Wedding/07.jpg";
+import wedding8 from "../assets/Wedding/08.jpg";
+import wedding9 from "../assets/Wedding/09.jpg";
+import wedding10 from "../assets/Wedding/10.jpg";
+import wedding11 from "../assets/Wedding/11.jpg";
+import wedding12 from "../assets/Wedding/12.jpg";
+import wedding13 from "../assets/Wedding/13.jpg";
+import wedding14 from "../assets/Wedding/14.jpg";
+import wedding15 from "../assets/Wedding/15.jpg";
+import wedding16 from "../assets/Wedding/16.jpg";
+import wedding17 from "../assets/Wedding/17.jpg";
+import wedding18 from "../assets/Wedding/18.jpg";
+import wedding19 from "../assets/Wedding/19.jpg";
+import wedding20 from "../assets/Wedding/20.jpg";
+import wedding21 from "../assets/Wedding/21.jpg";
+import wedding22 from "../assets/Wedding/22.jpg";
+import wedding23 from "../assets/Wedding/23.jpg";
+import wedding24 from "../assets/Wedding/24.jpg";
+import wedding25 from "../assets/Wedding/25.jpg";
+import wedding26 from "../assets/Wedding/26.jpg";
+import wedding27 from "../assets/Wedding/27.jpg";
+import wedding28 from "../assets/Wedding/28.jpg";
+import wedding29 from "../assets/Wedding/29.jpg";
+import wedding30 from "../assets/Wedding/30.jpg";
+import wedding31 from "../assets/Wedding/31.jpg";
+import wedding32 from "../assets/Wedding/32.jpg";
+import wedding33 from "../assets/Wedding/33.jpg";
+import wedding34 from "../assets/Wedding/34.jpg";
+import wedding35 from "../assets/Wedding/35.jpg";
+
+import puberty1 from "../assets/Puberty/pu1.jpg";
+import puberty2 from "../assets/Puberty/pu2.jpg";
+import puberty3 from "../assets/Puberty/pu3.jpg";
+import puberty4 from "../assets/Puberty/pu4.jpg";
+import puberty5 from "../assets/Puberty/pu5.jpg";
+import puberty6 from "../assets/Puberty/pu6.jpg";
+import puberty7 from "../assets/Puberty/pu7.jpg";
+import puberty8 from "../assets/Puberty/pu8.jpg";
+import puberty9 from "../assets/Puberty/pu9.jpg";
+import puberty10 from "../assets/Puberty/pu10.jpg";
+import puberty11 from "../assets/Puberty/pu11.jpg";
+import puberty12 from "../assets/Puberty/pu12.jpg";
+import puberty13 from "../assets/Puberty/pu13.jpg";
+import puberty14 from "../assets/Puberty/pu14.jpg";
+import puberty15 from "../assets/Puberty/pu15.jpg";
+import puberty16 from "../assets/Puberty/pu16.jpg";
+import puberty17 from "../assets/Puberty/pu17.jpg";
+import puberty18 from "../assets/Puberty/pu18.jpg";
+import puberty19 from "../assets/Puberty/pu19.jpg";
+import puberty20 from "../assets/Puberty/pu20.jpg";
+import puberty21 from "../assets/Puberty/pu21.jpg";
+import puberty22 from "../assets/Puberty/pu22.jpg";
+import puberty23 from "../assets/Puberty/pu23.jpg";
+import puberty24 from "../assets/Puberty/pu24.jpg";
+import puberty25 from "../assets/Puberty/pu25.jpg";
+import puberty26 from "../assets/Puberty/pu26.jpg";
+import puberty27 from "../assets/Puberty/pu27.jpg";
+import puberty28 from "../assets/Puberty/pu28.jpg";
+import puberty29 from "../assets/Puberty/pu29.jpg";
+import puberty30 from "../assets/Puberty/pu30.jpg";
+import puberty31 from "../assets/Puberty/pu31.jpg";
+import puberty32 from "../assets/Puberty/pu32.jpg";
+import puberty33 from "../assets/Puberty/pu33.jpg";
+import puberty34 from "../assets/Puberty/pu34.jpg";
+import puberty35 from "../assets/Puberty/pu35.jpg";
+import puberty36 from "../assets/Puberty/pu36.jpg";
+import puberty37 from "../assets/Puberty/pu37.jpg";
+import puberty38 from "../assets/Puberty/pu38.jpg";
+import puberty39 from "../assets/Puberty/pu39.jpg";
+import puberty40 from "../assets/Puberty/pu40.jpg";
+import puberty41 from "../assets/Puberty/pu41.jpg";
+import puberty42 from "../assets/Puberty/pu42.jpg";
+import puberty43 from "../assets/Puberty/pu43.jpg";
+
+import maternity1 from "../assets/Maternity/m1.jpg";
+import maternity2 from "../assets/Maternity/m2.jpg";
+import maternity3 from "../assets/Maternity/m3.jpg";
+import maternity4 from "../assets/Maternity/m4.jpg";
+import maternity5 from "../assets/Maternity/m5.jpg";
+import maternity6 from "../assets/Maternity/m6.jpg";
+import maternity7 from "../assets/Maternity/m7.jpg";
+import maternity8 from "../assets/Maternity/m8.jpg";
+import maternity9 from "../assets/Maternity/m9.jpg";
+import maternity10 from "../assets/Maternity/m10.jpg";
+import maternity11 from "../assets/Maternity/m11.jpg";
+import maternity12 from "../assets/Maternity/m12.jpg";
+import maternity13 from "../assets/Maternity/m13.jpg";
+import maternity14 from "../assets/Maternity/m14.jpg";
+import maternity15 from "../assets/Maternity/m15.jpg";
+import maternity16 from "../assets/Maternity/m16.jpg";
+import maternity17 from "../assets/Maternity/m17.jpg";
+import maternity18 from "../assets/Maternity/m18.jpg";
+import maternity19 from "../assets/Maternity/m19.jpg";
+import maternity20 from "../assets/Maternity/m20.jpg";
+import maternity21 from "../assets/Maternity/m21.jpg";
+import maternity22 from "../assets/Maternity/m22.jpg";
+import maternity23 from "../assets/Maternity/m23.jpg";
+import maternity24 from "../assets/Maternity/m24.jpg";
+
+import birthday1 from "../assets/Birthday/b1.jpg";
+import birthday2 from "../assets/Birthday/b2.jpg";
+import birthday3 from "../assets/Birthday/b3.jpg";
+import birthday4 from "../assets/Birthday/b4.jpg";
+import birthday5 from "../assets/Birthday/b5.jpg";
+import birthday6 from "../assets/Birthday/b6.jpg";
+import birthday7 from "../assets/Birthday/b7.jpg";
+import birthday8 from "../assets/Birthday/b8.jpg";
+import birthday9 from "../assets/Birthday/b9.jpg";
+import birthday10 from "../assets/Birthday/b10.jpg";
+import birthday11 from "../assets/Birthday/b11.jpg";
+import birthday12 from "../assets/Birthday/b12.jpg";
+import birthday13 from "../assets/Birthday/b13.jpg";
+import birthday14 from "../assets/Birthday/b14.jpg";
+import birthday15 from "../assets/Birthday/b15.jpg";
+import birthday16 from "../assets/Birthday/b16.jpg";
+import birthday17 from "../assets/Birthday/b17.jpg";
+import birthday18 from "../assets/Birthday/b18.jpg";
+import birthday19 from "../assets/Birthday/b19.jpg";
+import birthday20 from "../assets/Birthday/b20.jpg";
+import birthday21 from "../assets/Birthday/b21.jpg";
+import birthday22 from "../assets/Birthday/b22.jpg";
+import birthday23 from "../assets/Birthday/b23.jpg";
+import birthday24 from "../assets/Birthday/b24.jpg";
+import birthday25 from "../assets/Birthday/b25.jpg";
+import birthday26 from "../assets/Birthday/b26.jpg";
+import birthday27 from "../assets/Birthday/b27.jpg";
+import birthday28 from "../assets/Birthday/b28.jpg";
+import birthday29 from "../assets/Birthday/b29.jpg";
+import birthday30 from "../assets/Birthday/b30.jpg";
+import birthday31 from "../assets/Birthday/b31.jpg";
+import birthday32 from "../assets/Birthday/b32.jpg";
+import birthday33 from "../assets/Birthday/b33.jpg";
+import birthday34 from "../assets/Birthday/b34.jpg";
+import birthday35 from "../assets/Birthday/b35.jpg";
+import birthday36 from "../assets/Birthday/b36.jpg";
+import birthday37 from "../assets/Birthday/b37.jpg";
+import birthday38 from "../assets/Birthday/b38.jpg";
+import birthday39 from "../assets/Birthday/b39.jpg";
+import birthday40 from "../assets/Birthday/b40.jpg";
+import birthday41 from "../assets/Birthday/b41.jpg";
+import birthday42 from "../assets/Birthday/b42.jpg";
+
+import bridal1 from "../assets/Bridal/mo1.jpg";
+import bridal2 from "../assets/Bridal/mo2.jpg";
+import bridal3 from "../assets/Bridal/mo3.jpg";
+import bridal4 from "../assets/Bridal/mo4.jpg";
+import bridal5 from "../assets/Bridal/mo5.jpg";
+import bridal6 from "../assets/Bridal/mo6.jpg";
+import bridal7 from "../assets/Bridal/mo7.jpg";
+import bridal8 from "../assets/Bridal/mo8.jpg";
+import bridal9 from "../assets/Bridal/mo9.jpg";
+import bridal10 from "../assets/Bridal/mo10.jpg";
+import bridal11 from "../assets/Bridal/mo11.jpg";
+import bridal12 from "../assets/Bridal/mo12.jpg";
+import bridal13 from "../assets/Bridal/mo13.jpg";
+import bridal14 from "../assets/Bridal/mo14.jpg";
+import bridal15 from "../assets/Bridal/mo15.jpg";
+import bridal16 from "../assets/Bridal/mo16.jpg";
+import bridal17 from "../assets/Bridal/mo17.jpg";
+import bridal18 from "../assets/Bridal/mo18.jpg";
+
+import product1 from "../assets/Product/p1.jpg";
+import product2 from "../assets/Product/p2.jpg";
+import product3 from "../assets/Product/p3.jpg";
+import product4 from "../assets/Product/p4.jpg";
+import product5 from "../assets/Product/p5.jpg";
+import product6 from "../assets/Product/p6.jpg";
+import product7 from "../assets/Product/p7.jpg";
+import product8 from "../assets/Product/p8.jpg";
+import product9 from "../assets/Product/p9.jpg";
+import product10 from "../assets/Product/p10.jpg";
+import product11 from "../assets/Product/p11.jpg";
+import product12 from "../assets/Product/p12.jpg";
+import product13 from "../assets/Product/p13.jpg";
+import product14 from "../assets/Product/p14.jpg";
+import product15 from "../assets/Product/p15.jpg";
+
+import temple1 from "../assets/Temple/k1.JPG";
+import temple2 from "../assets/Temple/k2.JPG";
+import temple3 from "../assets/Temple/k3.JPG";
+import temple4 from "../assets/Temple/k4.JPG";
+import temple5 from "../assets/Temple/k5.JPG";
+import temple6 from "../assets/Temple/k6.JPG";
+import temple7 from "../assets/Temple/k7.JPG";
+import temple8 from "../assets/Temple/k8.JPG";
+import temple9 from "../assets/Temple/k9.JPG";
+import temple10 from "../assets/Temple/k10.JPG";
+import temple11 from "../assets/Temple/k11.JPG";
+import temple12 from "../assets/Temple/k12.JPG";
+import temple13 from "../assets/Temple/k13.JPG";
+import temple14 from "../assets/Temple/k14.JPG";
+import temple15 from "../assets/Temple/k15.JPG";
+
+
+
+
+
+
+
+
+
 export const galleryImages = {
-    "wedding-engagement": [
-      { type: "image", src: "/images/wedding1.jpg" },
-      { type: "image", src: "/images/wedding2.jpg" },
-    ],
-    "video-edits": [
-      { type: "video", src: "/videos/edit1.mp4" },
-      { type: "video", src: "/videos/edit2.mp4" },
-      { type: "video", src: "/videos/edit3.mp4" },
-    ],
-    // other categories...
-  };
+  "wedding-engagement": [
+    { type: "image", src: wedding1 },
+    { type: "image", src: wedding2 },
+    { type: "image", src: wedding3 },
+    { type: "image", src: wedding4 },
+    { type: "image", src: wedding5 },
+    { type: "image", src: wedding6 },
+    { type: "image", src: wedding7 },
+    { type: "image", src: wedding8 },
+    { type: "image", src: wedding9 },
+    { type: "image", src: wedding10 },
+    { type: "image", src: wedding11 },
+    { type: "image", src: wedding12 },
+    { type: "image", src: wedding13 },
+    { type: "image", src: wedding14 },
+    { type: "image", src: wedding15 },
+    { type: "image", src: wedding16 },
+    { type: "image", src: wedding17 },
+    { type: "image", src: wedding18 },
+    { type: "image", src: wedding19 },
+    { type: "image", src: wedding20 },
+    { type: "image", src: wedding21 },
+    { type: "image", src: wedding22 },
+    { type: "image", src: wedding23 },
+    { type: "image", src: wedding24 },
+    { type: "image", src: wedding25 },
+    { type: "image", src: wedding26 },
+    { type: "image", src: wedding27 },
+    { type: "image", src: wedding28 },
+    { type: "image", src: wedding29 },
+    { type: "image", src: wedding30 },
+    { type: "image", src: wedding31 },
+    { type: "image", src: wedding32 },
+    { type: "image", src: wedding33 },
+    { type: "image", src: wedding34 },
+    { type: "image", src: wedding35 }
+  ],
+  "puberty-ceremony": [
+    { type: "image", src: puberty1 },
+{ type: "image", src: puberty2 },
+{ type: "image", src: puberty3 },
+{ type: "image", src: puberty4 },
+{ type: "image", src: puberty5 },
+{ type: "image", src: puberty6 },
+{ type: "image", src: puberty7 },
+{ type: "image", src: puberty8 },
+{ type: "image", src: puberty9 },
+{ type: "image", src: puberty10 },
+{ type: "image", src: puberty11 },
+{ type: "image", src: puberty12 },
+{ type: "image", src: puberty13 },
+{ type: "image", src: puberty14 },
+{ type: "image", src: puberty15 },
+{ type: "image", src: puberty16 },
+{ type: "image", src: puberty17 },
+{ type: "image", src: puberty18 },
+{ type: "image", src: puberty19 },
+{ type: "image", src: puberty20 },
+{ type: "image", src: puberty21 },
+{ type: "image", src: puberty22 },
+{ type: "image", src: puberty23 },
+{ type: "image", src: puberty24 },
+{ type: "image", src: puberty25 },
+{ type: "image", src: puberty26 },
+{ type: "image", src: puberty27 },
+{ type: "image", src: puberty28 },
+{ type: "image", src: puberty29 },
+{ type: "image", src: puberty30 },
+{ type: "image", src: puberty31 },
+{ type: "image", src: puberty32 },
+{ type: "image", src: puberty33 },
+{ type: "image", src: puberty34 },
+{ type: "image", src: puberty35 },
+{ type: "image", src: puberty36 },
+{ type: "image", src: puberty37 },
+{ type: "image", src: puberty38 },
+{ type: "image", src: puberty39 },
+{ type: "image", src: puberty40 },
+{ type: "image", src: puberty41 },
+{ type: "image", src: puberty42 },
+{ type: "image", src: puberty43 },
+  ],
+  "maternity-shoot": [
+    { type: "image", src: maternity1 },
+    { type: "image", src: maternity2 },
+    { type: "image", src: maternity3 },
+    { type: "image", src: maternity4 },
+    { type: "image", src: maternity5 },
+    { type: "image", src: maternity6 },
+    { type: "image", src: maternity7 },
+    { type: "image", src: maternity8 },
+    { type: "image", src: maternity9 },
+    { type: "image", src: maternity10 },
+    { type: "image", src: maternity11 },
+    { type: "image", src: maternity12 },
+    { type: "image", src: maternity13 },
+    { type: "image", src: maternity14 },
+    { type: "image", src: maternity15 },
+    { type: "image", src: maternity16 },
+    { type: "image", src: maternity17 },
+    { type: "image", src: maternity18 },
+    { type: "image", src: maternity19 },
+    { type: "image", src: maternity20 },
+    { type: "image", src: maternity21 },
+    { type: "image", src: maternity22 },
+    { type: "image", src: maternity23 },
+    { type: "image", src: maternity24 },
+
+    
+  ],
+  "birthday-shoot": [
+    { type: "image", src: birthday1 },
+{ type: "image", src: birthday2 },
+{ type: "image", src: birthday3 },
+{ type: "image", src: birthday4 },
+{ type: "image", src: birthday5 },
+{ type: "image", src: birthday6 },
+{ type: "image", src: birthday7 },
+{ type: "image", src: birthday8 },
+{ type: "image", src: birthday9 },
+{ type: "image", src: birthday10 },
+{ type: "image", src: birthday11 },
+{ type: "image", src: birthday12 },
+{ type: "image", src: birthday13 },
+{ type: "image", src: birthday14 },
+{ type: "image", src: birthday15 },
+{ type: "image", src: birthday16 },
+{ type: "image", src: birthday17 },
+{ type: "image", src: birthday18 },
+{ type: "image", src: birthday19 },
+{ type: "image", src: birthday20 },
+{ type: "image", src: birthday21 },
+{ type: "image", src: birthday22 },
+{ type: "image", src: birthday23 },
+{ type: "image", src: birthday24 },
+{ type: "image", src: birthday25 },
+{ type: "image", src: birthday26 },
+{ type: "image", src: birthday27 },
+{ type: "image", src: birthday28 },
+{ type: "image", src: birthday29 },
+{ type: "image", src: birthday30 },
+{ type: "image", src: birthday31 },
+{ type: "image", src: birthday32 },
+{ type: "image", src: birthday33 },
+{ type: "image", src: birthday34 },
+{ type: "image", src: birthday35 },
+{ type: "image", src: birthday36 },
+{ type: "image", src: birthday37 },
+{ type: "image", src: birthday38 },
+{ type: "image", src: birthday39 },
+{ type: "image", src: birthday40 },
+{ type: "image", src: birthday41 },
+{ type: "image", src: birthday42 },
+
+
+  ],
+  "bridal-photography": [
+    { type: "image", src: bridal1 },
+{ type: "image", src: bridal2 },
+{ type: "image", src: bridal3 },
+{ type: "image", src: bridal4 },
+{ type: "image", src: bridal5 },
+{ type: "image", src: bridal6 },
+{ type: "image", src: bridal7 },
+{ type: "image", src: bridal8 },
+{ type: "image", src: bridal9 },
+{ type: "image", src: bridal10 },
+{ type: "image", src: bridal11 },
+{ type: "image", src: bridal12 },
+{ type: "image", src: bridal13 },
+{ type: "image", src: bridal14 },
+{ type: "image", src: bridal15 },
+{ type: "image", src: bridal16 },
+{ type: "image", src: bridal17 },
+{ type: "image", src: bridal18 },
+
+  ],
+  "product-photography": [
+    { type: "image", src: product1 },
+{ type: "image", src: product2 },
+{ type: "image", src: product3 },
+{ type: "image", src: product4 },
+{ type: "image", src: product5 },
+{ type: "image", src: product6 },
+{ type: "image", src: product7 },
+{ type: "image", src: product8 },
+{ type: "image", src: product9 },
+{ type: "image", src: product10 },
+{ type: "image", src: product11 },
+{ type: "image", src: product12 },
+{ type: "image", src: product13 },
+{ type: "image", src: product14 },
+{ type: "image", src: product15 },
+
+  ],
+  "temple-photography": [
+    { type: "image", src: temple1 },
+{ type: "image", src: temple2 },
+{ type: "image", src: temple3 },
+{ type: "image", src: temple4 },
+{ type: "image", src: temple5 },
+{ type: "image", src: temple6 },
+{ type: "image", src: temple7 },
+{ type: "image", src: temple8 },
+{ type: "image", src: temple9 },
+{ type: "image", src: temple10 },
+{ type: "image", src: temple11 },
+{ type: "image", src: temple12 },
+{ type: "image", src: temple13 },
+{ type: "image", src: temple14 },
+{ type: "image", src: temple15 },
+
+  ],
   
+};
